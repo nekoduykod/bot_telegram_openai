c3b061152cdf34fd09b73aad7e87088bd4c9268d
Oleksandr <zaliubovskyio@ukr.net>
2024-02-01 02:11:44 +0200

    good dialogue

--- bot/handlers/handler.py
+++ bot/handlers/handler.py
@@ -4,32 +4,32 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.markdown import hbold
-from aiogram.types import Message
 
 from bot_instance import bot
 from bot.keyboards import locations_kb
 
-import openai
-
 
 storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage) 
+dp = Dispatcher(bot, storage=storage)
 
 
 class DialogueStates(StatesGroup):
-    Location = State() # Will be represented in storage as 'DialogueStates:Location'
-    Checklist = State() # Will be represented in storage as 'DialogueStates:Checklist'
+    Location = State()  # Will be represented in storage as 'DialogueStates:Location'
+    Checklist = State()  # Will be represented in storage as 'DialogueStates:Checklist'
     WaitingForChoice = State()
 
-
-class ChecklistState(StatesGroup):
-    waiting_for_choice = State()
+class ChecklistForm(StatesGroup):
+    Item1 = State()
+    Item2 = State()
+    Item3 = State()
+    Item4 = State()
+    Item5 = State()
 
 
 @dp.message_handler(Command('start'))
 async def welcome(message: types.Message, state: FSMContext) -> None:
-    """ the command 'start' """
-    reply_text = f'{hbold(message.from_user.first_name)}, ласкаво прошу. Почнімо'
+    """ The command 'start' """
+    reply_text = f'{hbold(message.from_user.first_name)}, ласкаво прошу. Почнімо. Оберіть локацію, що вас цікавить, будь ласка.'
 
     await bot.send_message(
         message.chat.id,
@@ -39,34 +39,105 @@ async def welcome(message: types.Message, state: FSMContext) -> None:
 
     await DialogueStates.Location.set()
 
-
 @dp.message_handler(lambda message: message.text == 'Location 1', state=DialogueStates.Location)
 async def choose_location(message: types.Message, state: FSMContext):
     location = message.text
     await state.update_data(location=location)
 
-    await bot.send_message(message.chat.id, text = f"{location}. Чекліст! Введіть 1 - Залишити чистим | 2 - Залишити коментар.")
-    # Present the checklist
-    checklist = [{"Item": item, "Response": None} for item in ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]]
-    await state.update_data(checklist=checklist)
+    await bot.send_message(message.chat.id, text=f"{location}. Заповніть, будь ласка, чекліст")
+ 
+    await bot.send_message(message.chat.id, text="Item 1, введіть цифру 1 - Пропустити | 2 - Залишити коментар.")
+    await ChecklistForm.Item1.set()
 
-    # Call the functions for each item in the checklist
-    for item in checklist:
-        await process_item(message, state, item)
+@dp.message_handler(state=ChecklistForm.Item1)
+async def process_item1(message: types.Message, state: FSMContext):
+    choice = message.text
 
-    await DialogueStates.WaitingForChoice.set()
+    if choice == '1':
+        # go to next item
+        await ChecklistForm.next()
+    elif choice == '2':
+        await bot.send_message(message.chat.id, text="Залиште коментар для Item 1")
+        await ChecklistForm.next()
+
+@dp.message_handler(state=ChecklistForm.Item2)
+async def process_item2(message: types.Message, state: FSMContext):
+    await bot.send_message(message.chat.id, text="Item 2, введіть цифру 1 - Пропустити | 2 - Залишити коментар.")
+    choice = message.text
+
+    if choice == '1':
+        # User chose to skip, move to the next item
+        await ChecklistForm.next()
+    elif choice == '2':
+        # User chose to leave a comment, ask for comment
+        await bot.send_message(message.chat.id, text="Залиште коментар для Item 2")
+        await ChecklistForm.next()
 
 
-async def process_item(message: types.Message, state: FSMContext, item):
-    await bot.send_message(message.chat.id, text = f"{item['Item']}, введіть цифру 1 | 2:")
-    user_choice = await bot.wait_for_new_message(chat_id=message.chat.id)
+@dp.message_handler(state=ChecklistForm.Item3)
+async def process_item3(message: types.Message, state: FSMContext):
+    await bot.send_message(message.chat.id, text="Item 3, введіть цифру 1 - Пропустити | 2 - Залишити коментар.")
+    choice = message.text
 
-    if user_choice.text == '2':
-        await bot.send_message(message.chat.id, text = f"Будь ласка, залиши коментар.")
-        user_input = await bot.wair_for(chat_id=message.chat.id)
-        item["Response"] = user_input.text
-    else:
-        pass
+    if choice == '1':
+        # User chose to skip, move to the next item
+        await ChecklistForm.next()
+    elif choice == '2':
+        # User chose to leave a comment, ask for comment
+        await bot.send_message(message.chat.id, text="Залиште коментар для Item 3")
+        await ChecklistForm.next()
+
+
+@dp.message_handler(state=ChecklistForm.Item4)
+async def process_item4(message: types.Message, state: FSMContext):
+    await bot.send_message(message.chat.id, text="Item 4, введіть цифру 1 - Пропустити | 2 - Залишити коментар.")
+    choice = message.text
+
+    if choice == '1':
+        # User chose to skip, move to the next item
+        await ChecklistForm.next()
+    elif choice == '2':
+        # User chose to leave a comment, ask for comment
+        await bot.send_message(message.chat.id, text="Залиште коментар для Item 4")
+        await ChecklistForm.next()
+
+
+@dp.message_handler(state=ChecklistForm.Item5)
+async def process_item5(message: types.Message, state: FSMContext):
+    await bot.send_message(message.chat.id, text="Item 5, введіть цифру 1 - Пропустити | 2 - Залишити коментар.")
+    choice = message.text
+
+    if choice == '1':
+        # User chose to skip, finish the checklist
+        await bot.send_message(message.chat.id, text="Чекліст завершено. Обробка результатів...")
+        await process_checklist_and_send_report(state)
+        await state.finish()
+    elif choice == '2':
+        # User chose to leave a comment, ask for comment
+        await bot.send_message(message.chat.id, text="Залиште коментар для Item 5:")
+        await process_checklist_and_send_report(state)
+
+
+async def process_checklist_and_send_report(state: FSMContext):
+    data = await state.get_data()
+    location = data.get('location')
+    checklist_responses = [
+        data.get('item1_response'),
+        data.get('item2_response'),
+        data.get('item3_response'),
+        data.get('item4_response'),
+        data.get('item5_response'),
+    ]
+    print(checklist_responses); print(location)
+    # Process the checklist data and send the report to OpenAI
+    # ...
+
+    # Send a completion message to the user
+
+    # Reset the state
+    await bot.send_message(state.user, text="Чекліст завершено. Дякуємо!")
+    await state.finish()
+
 
 # @dp.message_handler(state='*')
 # async def process_checklist(message: types.Message, state: FSMContext):
