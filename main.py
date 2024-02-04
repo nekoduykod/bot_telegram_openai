684a3bc5f1217765939ee596231a01fda1e9e026
Oleksandr <zaliubovskyio@ukr.net>
2024-02-04 03:06:16 +0200

    finished

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import asyncio
 
 from bot_instance import bot
-from aiogram import executor
-
 from bot.handlers.handler import dp
 
 async def main() -> None:
-    """It will execute our event loop and start polling."""
+    """it executes the event loop and start polling"""
     await dp.start_polling()
 
 if __name__ == '__main__':
-    # Start the bot 
+    # starts the bot 
     asyncio.run(main())
