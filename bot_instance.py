f23f59c493b22ed2a961d1e3440c2eab7f09a85e
Oleksandr <zaliubovskyio@ukr.net>
2024-02-04 03:06:29 +0200

    added comment expl

--- bot_instance.py
+++ bot_instance.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 
 from token_api import TOKEN_API
 
-# Створює бот інстанс
+# Створює бот інстанс. parse_mode='HTML' => щоб ваше ім'я при привітанні було читабельне
 bot = Bot(
     token=TOKEN_API,
     parse_mode='HTML'
