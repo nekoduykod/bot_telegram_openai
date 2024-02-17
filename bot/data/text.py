16befea9e6b9fa9b0827e157edb9c6b105cd11f9
Oleksandr <zaliubovskyio@ukr.net>
2024-02-17 02:48:10 +0200

    updated text, changed img

--- bot/data/text.py
+++ bot/data/text.py
@@ -14,11 +14,11 @@ num_one_two_text = "Ніт, лише цифра 1 чи 2, будь ласка."
 
 leave_photo_url_text = "Залиште URL на фото, що стосується питання. Ніт - 1. Так - 2"
 
-links_charities_text = '''❗ Donations are essential for Ukraine`s & Europe`s future.\n
+links_charities_text = '''❗ Donations are essential for Ukraine\`s & Europe\`s future.\n
 ❗ Russians continue their genocidal war against ukrainians, committing various atrocities.\n
 📝Popular trusted charities:\n
-➡️Come Back Alive: https://savelife.in.ua/donate/\n
-➡️Serhii Prytula Foundation: https://prytulafoundation.org/en\n
-➡️United24: https://u24.gov.ua/uk\n
-➡️3-a Штурмова Бригада: https://ab3.support/\n
-etc. Thank you.'''
+➡️[Come Back Alive](https://savelife.in.ua/donate/)\n
+➡️[Serhii Prytula Foundation](https://prytulafoundation.org/en\)\n
+➡️[United24](https://u24.gov.ua/uk)\n
+➡️[3-a Штурмова Бригада](https://ab3.support/)\n
+..etc. Thank you.'''
--- bot/handlers/users/donate_ua_army.py
+++ bot/handlers/users/donate_ua_army.py
@@ -7,4 +7,6 @@ from bot.data.text import links_charities_text
 @dp.message_handler(commands=['donate'])
 async def donate_links(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id, 
-                           text=links_charities_text)
+                           text=links_charities_text,
+                           parse_mode="Markdown",
+                           disable_web_page_preview=True)
