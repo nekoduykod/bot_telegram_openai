923dd703e845e26b7dc74576b45b12abddfbe62b
Oleksandr Zaliubovskyi <135022571+nekoduykod@users.noreply.github.com>
2024-02-21 01:42:09 +0200

    added 3+ver

--- bot/handlers/users/donate_ua_army.py
+++ bot/handlers/users/donate_ua_army.py
@@ -10,3 +10,24 @@ async def donate_links(message: types.Message):
                            text=links_charities_text,
                            parse_mode="Markdown",
                            disable_web_page_preview=True)
+
+# if 3+
+# from aiogram import Router
+# from aiogram.filters import Command
+# from aiogram.types import Message
+
+# from bot.data.text import links_charities_text
+# # from bot.utils.misc import rate_limit   . Obsolete. Actually, check middleware.throttling (rate_limit)
+
+
+# donate_router = Router()
+
+
+# # @rate_limit(limit=3)  # Anti-spam
+# @donate_router.message(Command(commands=['donate']))
+# async def donate_links(bot, message: Message):
+#     await message.delete()
+#     await bot.send_message(chat_id=message.from_user.id, 
+#                            text=links_charities_text,
+#                            parse_mode="Markdown",
+#                            disable_web_page_preview=True)
